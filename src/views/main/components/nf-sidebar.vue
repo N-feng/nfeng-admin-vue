ee92c2f2851135e8df84ab42f52dfad22a278828
聂杞锋 <308561157@qq.com>
2018-12-10 23:48:29 +0800

    调整样式细节

--- src/views/main/components/nf-sidebar.vue
+++ src/views/main/components/nf-sidebar.vue
@@ -1,9 +1,12 @@
 <template>
 <aside class="nf-sidebar nf-home__sidebar">
-    <nf-menu class="nf-menu" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+    <nf-menu class="nf-menu" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" :default-openeds="['0']">
         <!-- 一级栏目 -->
         <nf-submenu v-for="(item,key) in RouterModel[0].children" :key="key" :index="key + ''">
-            <template slot="title">{{ item.title }}</template>
+            <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ item.title }}</span>
+            </template>
             <!-- 二级栏目 -->
             <nf-menu-item v-for="(el,index) in item.children" :key="index">{{ el.title }}</nf-menu-item>
         </nf-submenu>
