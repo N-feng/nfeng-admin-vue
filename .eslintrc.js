1e251faaf60c35dc3210290cb1ec8d3f6fe4ad32
nfeng <nfeng@nfengdeMacBook-Pro.local>
2019-01-11 10:43:03 +0800

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,11 @@ module.exports = {
     'no-new': 0,// new Vue要用
     'no-underscore-dangle': 0,// 可使用下划线命名
     'prefer-promise-reject-errors': 0,// reject不强制Error
+    // 本项目的规则:
+    'no-restricted-globals': 0, // 禁用特定的全局变量
+    'no-param-reassign': 0, // 不允许对 function 的参数进行重新赋值
+    'consistent-return': 0, // 要求 return 语句要么总是指定返回的值，要么不指定
+    'no-multi-assign': 0, // 不允许在单个语句中使用多个分配
+    'no-unused-expressions': 0, // 禁止出现未使用过的表达式
   }
 }
