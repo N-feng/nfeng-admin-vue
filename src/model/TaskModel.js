633015b82b78cc7d1dba653ba19f467649099c25
nfeng <308561157@qq.com>
2019-05-22 10:02:58 +0800

    整理 任务页面 开始使用饿了么

---
+++ src/model/TaskModel.js
@@ -0,0 +1,10 @@
+class TaskModel {
+    constructor() {
+        this.title = ''
+        this.description = ''
+        this.date = ''
+        this.Priority = ''
+    }
+}
+
+export default TaskModel
--- src/views/admin/task/list.vue
+++ src/views/admin/task/list.vue
@@ -1,51 +1,43 @@
 <template>
     <nf-box title="taskList" border>
         <div slot="header__tools">
-            <nf-button type="primary" size="mini" @click="dialogFormVisible = true">new</nf-button>
+            <el-button type="primary" size="mini" @click="dialogFormVisible = true">new</el-button>
         </div>
-        <nf-dialog title="收货地址" :visible.sync="dialogFormVisible">
-            <nf-form :model="form">
-                <nf-form-item label="Title:">
-                    <nf-input v-model="form.name" autocomplete="off"></nf-input>
-                </nf-form-item>
-                <nf-form-item label="Description:">
-                    <nf-input v-model="form.name" autocomplete="off" type="textarea"></nf-input>
-                </nf-form-item>
-                <nf-form-item label="Set due date:">
-                    <el-date-picker v-model="value1" type="datetime" placeholder="选择日期时间"></el-date-picker>
-                </nf-form-item>
-                <nf-form-item label="Priority:">
-                    <nf-select v-model="form.region" placeholder="请选择活动区域">
-                        <nf-option label="区域一" value="shanghai"></nf-option>
-                        <nf-option label="区域二" value="beijing"></nf-option>
-                    </nf-select>
-                </nf-form-item>
-            </nf-form>
+        <el-dialog title="收货地址" :visible.sync="dialogFormVisible" append-to-body>
+            <el-form :model="TaskModel">
+                <el-form-item label="Title:">
+                    <el-input v-model="TaskModel.title" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="Description:">
+                    <el-input v-model="TaskModel.description" autocomplete="off" type="textarea"></el-input>
+                </el-form-item>
+                <el-form-item label="Set due date:">
+                    <el-date-picker v-model="TaskModel.date" type="datetime" placeholder="选择日期时间" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="Priority:">
+                    <el-select v-model="TaskModel.priority" placeholder="Select priority" style="width: 100%;">
+                        <el-option label="Low" value="Low"></el-option>
+                        <el-option label="Normal" value="Normal"></el-option>
+                        <el-option label="High" value="High"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
             <div slot="footer" class="dialog-footer">
-                <nf-button @click="dialogFormVisible = false">取 消</nf-button>
-                <nf-button type="primary" @click="dialogFormVisible = false">确 定</nf-button>
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
             </div>
-        </nf-dialog>
+        </el-dialog>
     </nf-box>
 </template>
 
 <script>
+import TaskModel from '../../../model/TaskModel'
+
 export default {
     data() {
         return {
             dialogFormVisible: false,
-            form: {
-                name: '',
-                region: '',
-                date1: '',
-                date2: '',
-                delivery: false,
-                type: [],
-                resource: '',
-                desc: '',
-            },
-            formLabelWidth: '120px',
-            value1: '',
+            TaskModel: new TaskModel(),
         }
     },
 }
