1c8daa46ca9aa5c066022d70c6f1ae59e31fc7c6
nfeng <308561157@qq.com>
2019-08-13 00:24:13 +0800

    继续开发markdown组件

--- src/components/NfMarkdown/index.vue
+++ src/components/NfMarkdown/index.vue
@@ -1,14 +1,174 @@
 <template>
-  <div>
+  <div class="markdown">
     <!-- 头部工具栏 -->
     <ul class="markdown-toolbars">
       <li>
         <slot name="title" />
       </li>
       <li name="粗体">
-        <span @click="insertStrong"
+        <span @click="insertMid('****')"
               class="iconfont icon-strong"></span>
       </li>
+      <li v-if="tools.italic"
+          name="斜体">
+        <span @click="insertMid('**')"
+              class="iconfont icon-italic"></span>
+      </li>
+      <li v-if="tools.overline"
+          name="删除线">
+        <span @click="insertMid('~~~~')"
+              class="iconfont icon-overline"></span>
+      </li>
+      <li v-if="tools.h1"
+          name="标题1">
+        <span style="font-size: 16px;"
+              @click="insertEnd(titleLevel[1])">h1</span>
+      </li>
+      <li v-if="tools.h2"
+          name="标题2">
+        <span style="font-size: 16px;"
+              @click="insertEnd(titleLevel[2])">h2</span>
+      </li>
+      <li v-if="tools.h3"
+          name="标题3">
+        <span style="font-size: 16px;"
+              @click="insertEnd(titleLevel[3])">h3</span>
+      </li>
+      <li v-if="tools.h4"
+          name="标题4">
+        <span style="font-size: 16px;"
+              @click="insertEnd(titleLevel[4])">h4</span>
+      </li>
+      <li v-if="tools.h5"
+          name="标题5">
+        <span style="font-size: 16px;"
+              @click="insertEnd(titleLevel[5])">h5</span>
+      </li>
+      <li v-if="tools.h6"
+          name="标题6">
+        <span style="font-size: 16px;"
+              @click="insertEnd(titleLevel[6])">h6</span>
+      </li>
+      <li v-if="tools.hr"
+          name="分割线">
+        <span @click="insertEnd('----')"
+              class="iconfont icon-horizontal"></span>
+      </li>
+      <li v-if="tools.quote"
+          name="引用">
+        <span style="font-size: 16px;"
+              @click="insertEnd('>  ')"
+              class="iconfont icon-quote"></span>
+      </li>
+      <li v-if="tools.ul"
+          name="无序列表">
+        <span @click="insertEnd('-  ')"
+              class="iconfont icon-ul"></span>
+      </li>
+      <li v-if="tools.ol"
+          name="有序列表">
+        <span @click="insertEnd('1. ')"
+              class="iconfont icon-ol"></span>
+      </li>
+      <li v-if="tools.code"
+          name="代码块">
+        <span @click="insertMid('```\n\n```')"
+              class="iconfont icon-code"></span>
+      </li>
+      <li v-if="tools.notChecked"
+          name="未完成列表">
+        <span @click="insertEnd('- [ ]  ')"
+              class="iconfont icon-checked-false"></span>
+      </li>
+      <li v-if="tools.checked"
+          name="已完成列表">
+        <span @click="insertEnd('- [x]  ')"
+              class="iconfont icon-checked"></span>
+      </li>
+      <li v-if="tools.link"
+          name="链接">
+        <span @click="insertEnd('[插入链接](href)')"
+              class="iconfont icon-link"></span>
+      </li>
+      <li v-if="tools.image"
+          name="图片">
+        <span @click="insertEnd('![image](imgUrl)')"
+              class="iconfont icon-img"></span>
+      </li>
+      <li v-if="tools.table"
+          name="表格">
+        <span @click="insertEnd('\nheader 1 | header 2\n---|---\nrow 1 col 1 | row 1 col 2\nrow 2 col 1 | row 2 col 2')"
+              class="iconfont icon-table"></span>
+      </li>
+      <!-- <li v-if="tools.print"
+          name="打印">
+        <span class="iconfont icon-dayin"
+              @click="print"></span>
+      </li> -->
+      <!-- <li v-if="tools.theme"
+          class="shift-theme"
+          name="代码块主题">
+        <div>
+          <span class="iconfont icon-yanse"
+                @click="themeSlideDown=!themeSlideDown"></span>
+          <ul :class="{active:themeSlideDown}"
+              @mouseleave="themeSlideDown=false">
+            <li @click="setThemes('Light')">Light</li>
+            <li @click="setThemes('Dark')">VS Code</li>
+            <li @click="setThemes('OneDark')">Atom OneDark</li>
+            <li @click="setThemes('GitHub')">GitHub</li>
+          </ul>
+        </div>
+      </li> -->
+      <!-- <li name="导入本地文件"
+          class="import-file"
+          v-show="tools.importmd">
+        <span class="iconfont icon-daoru"
+              @click="importFile"></span>
+        <input type="file"
+               @change="importFile($event)"
+               accept="text/markdown">
+      </li> -->
+      <!-- <li name="保存到本地"
+          v-show="tools.exportmd">
+        <span class="iconfont icon-download"
+              @click="exportMd"></span>
+      </li>
+      <li v-if="tools.shift&&preview==2"
+          name="全屏编辑">
+        <span @click="preview=3"
+              class="iconfont icon-md"></span>
+      </li>
+      <li v-if="tools.shift&&preview==3"
+          name="分屏显示">
+        <span @click="preview=1"
+              class="iconfont icon-group"></span>
+      </li>
+      <li v-if="tools.shift&&preview==1"
+          name="预览">
+        <span @click="preview=2"
+              class="iconfont icon-preview"></span>
+      </li>
+      <li :name="scrolling?'同步滚动:开':'同步滚动:关'">
+        <span @click="scrolling=!scrolling"
+              v-show="scrolling"
+              class="iconfont icon-on"></span>
+        <span @click="scrolling=!scrolling"
+              v-show="!scrolling"
+              class="iconfont icon-off"></span>
+      </li>
+      <li class="empty"></li>
+
+      <li v-if="tools.fullscreen&&!isFullscreen"
+          name="全屏">
+        <span @click="isFullscreen=!isFullscreen"
+              class="iconfont icon-full-screen"></span>
+      </li>
+      <li v-if="tools.fullscreen&&isFullscreen"
+          name="退出全屏">
+        <span @click="isFullscreen=!isFullscreen"
+              class="iconfont icon-exit-full-screen"></span>
+      </li> -->
     </ul>
     <!-- 编辑器 -->
     <div class="markdown-content">
@@ -20,7 +180,9 @@
               :key="index">{{ index + 1 }}</li>
         </ul>
         <textarea ref="textarea"
-                  v-model="value"></textarea>
+                  v-model="textareaValue"
+                  @input="handleInput"
+                  :style="{height:scrollHeight?`${scrollHeight}px`:'100%'}"></textarea>
       </div>
       <div class="empty"></div>
       <div ref="preview"
@@ -33,119 +195,12 @@
 </template>
 
 <script>
-import marked from 'marked'
+import markdown from './js/index'
 
-const renderer = new marked.Renderer()
-
-marked.setOptions({
-  renderer,
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: false,
-  smartLists: true,
-  // highlight(code) {
-  //   return hljs.highlightAuto(code).value
-  // },
-})
-
-export default {
-  name: 'NfMarkdown',
-  props: {
-    value: {},
-  },
-  data() {
-    return {
-      timeoutId: null,
-      indexLenth: 100,
-      html: '',
-      scrollHeight: null,
-    }
-  },
-  watch: {
-    value() {
-      clearTimeout(this.timeoutId)
-      this.timeoutId = setTimeout(() => {
-        this.html = marked(this.value, {
-          sanitize: false,
-          // ...this.markedOptions
-        })
-      }, 30)
-      this.indexLenth = this.value.split('\n').length
-      const height1 = this.indexLenth * 22
-      const height2 = this.$refs.textarea.scrollHeight
-      const height3 = this.$refs.preview.scrollHeight
-      this.scrollHeight = Math.max(height1, height2, height3)
-      this.indexLenth = parseInt(this.scrollHeight / 22, 0) - 1
-    },
-  },
-  methods: {
-    init() {
-      this.html = marked(this.value)
-    },
-  },
-  created() {
-    this.init()
-  },
-  destroyed() { // 销毁时清除定时器
-    clearInterval(this.timerId)
-  },
-}
+export default markdown
 </script>
 
-<style lang="scss" scoped>
-$line-height: 22px;
-.markdown-content {
-  height: 400px;
-  display: flex;
-  justify-content: space-between;
-  border-top: 1px solid #dddee1;
-}
-.markdown-editor {
-  flex: 1;
-  min-height: 100%;
-  overflow: hidden;
-  overflow-y: scroll;
-  display: flex;
-  justify-content: space-between;
-  background: #2d2d2d;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  .index {
-    background: #272727;
-    min-height: 100%;
-    width: 36px;
-    line-height: $line-height;
-    padding: 12px 0;
-
-    li {
-      background: #272727;
-      color: #ccc;
-      font-size: 14px;
-      text-align: center;
-    }
-  }
-
-  textarea {
-    width: 100%;
-    min-height: 100%;
-    outline: none;
-    border: 0;
-    background: #2d2d2d;
-    line-height: $line-height;
-    caret-color: #ccc;
-    color: #669acc;
-    font-size: 14px;
-    resize: none;
-    padding: 12px 8px;
-  }
-}
-.empty {
-  background: #f7f7f7;
-  width: 12px;
-}
+<style lang="scss">
+@import "./font/iconfont.css";
+@import "./scss/index.scss";
 </style>
---
+++ src/components/NfMarkdown/js/index.js
@@ -0,0 +1,230 @@
+
+import marked from 'marked'
+import defaultTools from './tools'
+
+const renderer = new marked.Renderer()
+
+marked.setOptions({
+  renderer,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  // highlight(code) {
+  //   return hljs.highlightAuto(code).value
+  // },
+})
+
+export default {
+  name: 'NfMarkdown',
+  props: {
+    value: {},
+    toolbars: { // 工具栏
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+  data() {
+    return {
+      textareaValue: '', // 输入框内容
+      timeoutId: null,
+      indexLenth: 100,
+      html: '',
+      scrollHeight: null,
+      themeName: '', // 主题名称
+      titleLevel: {
+        1: '#  ',
+        2: '##  ',
+        3: '###  ',
+        4: '####  ',
+        5: '#####  ',
+        6: '######  ',
+      },
+    }
+  },
+  computed: {
+    tools() {
+      const {
+        toolbars = {},
+      } = this
+      return {
+        ...defaultTools,
+        ...toolbars,
+      }
+    },
+  },
+  watch: {
+    textareaValue(val) {
+      clearTimeout(this.timeoutId)
+      this.timeoutId = setTimeout(() => {
+        this.html = marked(val, {
+          sanitize: false,
+          // ...this.markedOptions
+        })
+      }, 30)
+      this.indexLenth = this.value.split('\n').length
+      const height1 = this.indexLenth * 22
+      const height2 = this.$refs.textarea.scrollHeight
+      const height3 = this.$refs.preview.scrollHeight
+      this.scrollHeight = Math.max(height1, height2, height3)
+      this.indexLenth = parseInt(this.scrollHeight / 22, 0) - 1
+    },
+  },
+  methods: {
+    init() {
+      this.textareaValue = this.value
+      this.html = marked(this.textareaValue)
+    },
+    insertContent(initStr) { // 插入文本
+      const {
+        preview,
+      } = this
+      if (preview === 2) {
+        return
+      }
+      this.lastInsert = initStr
+      const point = this.getCursortPosition()
+      const value = this.textareaValue
+      const lastChart = value.substring(point - 1, point)
+      const lastFourCharts = value.substring(point - 4, point)
+      if (lastChart !== '\n' && value !== '' && lastFourCharts !== '    ') {
+        const str = `\n${initStr}`
+        this.insertAfterText(str)
+      } else {
+        this.insertAfterText(initStr)
+      }
+    },
+    getCursortPosition() { // 获取光标位置
+      const textDom = this.$refs.textarea
+      let cursorPos = 0
+      if (document.selection) {
+        textDom.focus()
+        const selectRange = document.selection.createRange()
+        selectRange.moveStart('character', -this.value.length)
+        cursorPos = selectRange.text.length
+      } else if (textDom.selectionStart || parseInt(textDom.selectionStart, 0) === 0) {
+        cursorPos = textDom.selectionStart
+      }
+      return cursorPos
+    },
+    insertAfterText(value) { // 插入文本
+      const textDom = this.$refs.textarea
+      let selectRange
+      if (document.selection) {
+        textDom.focus()
+        selectRange = document.selection.createRange()
+        selectRange.text = value
+        textDom.focus()
+      } else if (textDom.selectionStart || parseInt(textDom.selectionStart, 0) === 0) {
+        const startPos = textDom.selectionStart
+        const endPos = textDom.selectionEnd
+        // const { scrollTop } = textDom
+        this.textareaValue = textDom.value.substring(0, startPos) + value + textDom.value.substring(endPos, textDom.value.length)
+        // console.log(this.textareaValue)
+        textDom.value = textDom.value.substring(0, startPos) + value + textDom.value.substring(endPos, textDom.value.length)
+        // textDom.focus()
+        // textDom.selectionStart = startPos + value.length
+        // textDom.selectionEnd = startPos + value.length
+        // textDom.scrollTop = scrollTop
+      } else {
+        textDom.value += value
+        textDom.focus()
+      }
+      // this.$emit('input', textDom.value)
+    },
+    setCaretPosition(position) { // 设置光标位置
+      const textDom = this.$refs.textarea
+      if (textDom.setSelectionRange) {
+        textDom.focus()
+        textDom.setSelectionRange(position, position)
+      } else if (textDom.createTextRange) {
+        const range = textDom.createTextRange()
+        range.collapse(true)
+        range.moveEnd('character', position)
+        range.moveStart('character', position)
+        range.select()
+      }
+    },
+    insertMid(str) {
+      const point = this.getCursortPosition()
+      const value = this.textareaValue
+      const lastChart = value.substring(point - 1, point)
+      const exOff = lastChart !== '\n' && value !== '' ? 1 : 0 // 外偏移量(编辑框内换行等)
+      const newPoint = point + str.length / 2 + exOff // 光标新位置
+      this.insertContent(str)
+      this.setCaretPosition(newPoint)
+    },
+    insertEnd(str) {
+      const point = this.getCursortPosition()
+      const value = this.textareaValue
+      const lastChart = value.substring(point - 1, point)
+      this.insertContent(str)
+      if (lastChart !== '\n' && value !== '') {
+        this.setCaretPosition(point + str.length + 1)
+      } else {
+        this.setCaretPosition(point + str.length)
+      }
+    },
+    insertStrong() { // 粗体
+      const point = this.getCursortPosition()
+      const value = this.textareaValue
+      const lastChart = value.substring(point - 1, point)
+      this.insertContent('****')
+      if (lastChart !== '\n' && value !== '') {
+        this.setCaretPosition(point + 3)
+      } else {
+        this.setCaretPosition(point + 2)
+      }
+    },
+    insertItalic() { // 斜体
+      const point = this.getCursortPosition()
+      const value = this.textareaValue
+      const lastChart = value.substring(point - 1, point)
+      this.insertContent('**')
+      if (lastChart !== '\n' && value !== '') {
+        this.setCaretPosition(point + 2)
+      } else {
+        this.setCaretPosition(point + 1)
+      }
+    },
+    insertOverline() { // overline
+      const point = this.getCursortPosition()
+      const value = this.textareaValue
+      const lastChart = value.substring(point - 1, point)
+      this.insertContent('~~~~')
+      if (lastChart !== '\n' && value !== '') {
+        this.setCaretPosition(point + 3)
+      } else {
+        this.setCaretPosition(point + 2)
+      }
+    },
+    insertTitle(level) { // 插入标题
+      const titleLevel = {
+        1: '#  ',
+        2: '##  ',
+        3: '###  ',
+        4: '####  ',
+        5: '#####  ',
+        6: '######  ',
+      }
+      this.insertContent(titleLevel[level])
+    },
+    handleInput(e) {
+      const { value } = e.target
+      this.html = marked(value, {
+        sanitize: false,
+        // ...this.markedOptions
+      })
+    },
+  },
+  created() {
+    this.init()
+  },
+  destroyed() { // 销毁时清除定时器
+    clearInterval(this.timerId)
+  },
+}
