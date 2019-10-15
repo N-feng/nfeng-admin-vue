33ab4104ab658665e09143e77d818ce77fb2796b
nfeng <308561157@qq.com>
2019-10-15 17:33:07 +0800

    再改一下路由，头顶目录便捷一点

--- src/router/modules/admin.js
+++ src/router/modules/admin.js
@@ -1,5 +1,5 @@
 const Layout = () => import('@/components/Layout/admin.vue')
-const NfRouterView = () => import('@/components/NfRouterView')
+// const NfRouterView = () => import('@/components/NfRouterView')
 
 export const admin = {
   path: '/admin',
@@ -7,121 +7,90 @@ export const admin = {
   meta: {
     title: 'admin',
   },
-  redirect: '/admin/system',
+  redirect: '/admin/Landing',
   component: Layout,
   children: [
     {
-      path: 'system',
-      name: 'AdminManager',
+      path: 'Landing',
+      name: 'Landing',
       meta: {
-        title: 'AdminManager',
+        title: 'Landing',
       },
-      redirect: '/admin/system/Landing',
-      component: NfRouterView,
-      children: [
-        {
-          path: 'Landing',
-          name: 'Landing',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'Landing',
-          },
-          component: () => import('@/views/admin/index.vue'),
-        },
-        {
-          path: 'AuthManager',
-          name: 'AuthManager',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'AuthManager',
-          },
-          component: () => import('@/views/admin/auth/list.vue'),
-        },
-        {
-          path: 'RoleManager',
-          name: 'RoleManager',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'RoleManager',
-          },
-          component: () => import('@/views/admin/role/list.vue'),
-        },
-        {
-          path: 'TaskManager',
-          name: 'TaskManager',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'TaskManager',
-          },
-          component: () => import('@/views/admin/task/list.vue'),
-        },
-        {
-          path: 'NoteManagerList',
-          name: 'NoteManagerList',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'NoteManagerList',
-          },
-          component: () => import('@/views/admin/note/list.vue'),
-        },
-        {
-          path: 'NoteManagerCreate',
-          name: 'NoteManagerCreate',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'NoteManagerCreate',
-            rename: 'NoteManagerList',
-          },
-          component: () => import('@/views/admin/note/create.vue'),
-        },
-        {
-          path: 'ImgManagerList',
-          name: 'ImgManagerList',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'ImgManagerList',
-          },
-          component: () => import('@/views/admin/img/list.vue'),
-        },
-        {
-          path: 'ProjectManagerList',
-          name: 'ProjectManagerList',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'ProjectManagerList',
-          },
-          component: () => import('@/views/admin/project/list.vue'),
-        },
-        {
-          path: 'ProjectManagerCreate',
-          name: 'ProjectManagerCreate',
-          parentName: 'AdminManager',
-          meta: {
-            title: 'ProjectManagerCreate',
-            rename: 'ProjectManagerList',
-          },
-          component: () => import('@/views/admin/project/create.vue'),
-        },
-      ],
+      component: () => import('@/views/admin/index.vue'),
+    },
+    {
+      path: 'AuthManager',
+      name: 'AuthManager',
+      meta: {
+        title: 'AuthManager',
+      },
+      component: () => import('@/views/admin/auth/list.vue'),
+    },
+    {
+      path: 'RoleManager',
+      name: 'RoleManager',
+      meta: {
+        title: 'RoleManager',
+      },
+      component: () => import('@/views/admin/role/list.vue'),
+    },
+    {
+      path: 'TaskManager',
+      name: 'TaskManager',
+      meta: {
+        title: 'TaskManager',
+      },
+      component: () => import('@/views/admin/task/list.vue'),
+    },
+    {
+      path: 'ImgManager',
+      name: 'ImgManager',
+      meta: {
+        title: 'ImgManager',
+      },
+      component: () => import('@/views/admin/img/list.vue'),
+    },
+    {
+      path: 'NoteManagerList',
+      name: 'NoteManagerList',
+      meta: {
+        title: 'NoteManager',
+      },
+      component: () => import('@/views/admin/note/list.vue'),
+    },
+    {
+      path: 'NoteManagerCreate',
+      name: 'NoteManagerCreate',
+      meta: {
+        title: 'NoteManagerCreate',
+        rename: 'NoteManagerList',
+      },
+      component: () => import('@/views/admin/note/create.vue'),
+    },
+    {
+      path: 'ProjectManagerList',
+      name: 'ProjectManagerList',
+      meta: {
+        title: 'ProjectManager',
+      },
+      component: () => import('@/views/admin/project/list.vue'),
+    },
+    {
+      path: 'ProjectManagerCreate',
+      name: 'ProjectManagerCreate',
+      meta: {
+        title: 'ProjectManagerCreate',
+        rename: 'ProjectManagerList',
+      },
+      component: () => import('@/views/admin/project/create.vue'),
     },
     // {
-    //   path: 'note',
-    //   name: 'NoteManager',
+    //   path: 'system',
+    //   name: 'AdminManager',
     //   meta: {
-    //     title: 'NoteManager',
+    //     title: 'AdminManager',
     //   },
-    //   redirect: '/admin/note/list',
-    //   component: NfRouterView,
-    //   children: [
-    //   ],
-    // },
-    // {
-    //   path: 'project',
-    //   name: 'ProjectManager',
-    //   meta: {
-    //     title: 'ProjectManager',
-    //   },
-    //   redirect: '/admin/project/list',
+    //   redirect: '/admin/system/Landing',
     //   component: NfRouterView,
     //   children: [
     //   ],
