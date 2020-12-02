4bdaa3e61f0e27e2ec011a479492be3ac78bf3c3
N-feng <308561157@qq.com>
2020-12-02 16:24:35 +0800

    加上graphQL

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,26 @@ import router from "./router";
 import store from "./store";
 import "./plugins";
 
+import ApolloClient from 'apollo-boost'
+
+const apolloClient = new ApolloClient({
+  // 你需要在这里使用绝对路径
+  uri: 'http://localhost:3009/graphql'
+})
+
+import VueApollo from 'vue-apollo'
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider,
   store,
   render: (h) => h(App),
 }).$mount("#app");
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,121 @@
 <template>
   <div class="home">
-    {{ version }}
+    <div>{{ version }}</div>
+    <!-- <div>{{ findUser }}</div>
+    <div>{{ userList }}</div> -->
     <router-view />
     <!-- <a-button @click="$router.push('/micro-react/child-one')">back home</a-button> -->
+    <!-- <div style="margin-top: 10px;">
+      <a-button @click="getData">点击按钮触发时间请求graphQl接口</a-button>
+    </div>
+    <div style="margin-top: 10px;">
+      <a-button @click="loadMore">加载更多</a-button>
+    </div>
+    <div style="margin-top: 10px;">
+      <a-button @click="doAdd">添加</a-button>
+    </div> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { State } from 'vuex-class';
+import gql from 'graphql-tag'
+// import gqlTranslations from './translations.gql';
+// import "vue-apollo"
+
+const findUser = gql`query findUser($id:String!){
+  findUser(id: $id){
+    _id,
+    username,
+    createdAt,
+    updatedAt
+  }
+}
+`;
+
+const userList = gql`
+  query userList{
+    userList{
+      _id,
+      username,
+      createdAt,
+      updatedAt
+    }
+  }
+`;
+
+const addUser = gql`mutation($username:String!){
+  addUser(user:{username: $username}){
+    _id,
+    username,
+    createdAt,
+    updatedAt
+  }
+}
+`;
 
 const namespace = 'global';
 
-@Component
+@Component({
+  // apollo: {
+  //   userList() {
+  //     console.log(this.page)
+  //     return {
+  //       query: userList
+  //     }
+  //   },
+  // }
+})
 export default class Home extends Vue {
   @State('version', { namespace }) private version?: string;
+  // private translations = {};
+  private page = 1;
+  private findUser = {};
+  doAdd() {
+    this.$apollo.mutate({
+      mutation: addUser,
+      variables: {
+        username: '111'
+      }
+    }).then((response)=>{
+      console.log(response);
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  getData() {
+    // console.log(this.userList)
+    // console.log(this.$apollo);
+    this.$apollo.addSmartQuery('findUser', {
+      query: findUser,
+      variables: {
+        id: '5f7c380818cdaf11aedcdabd'
+      },
+      result(responese) {
+        console.log('responese: ', responese);
+      },
+      error(err) {
+        console.log('err: ', err);
+      }
+    })
+  }
+  loadMore() {
+    this.$apollo.queries.userList.fetchMore({
+      // 用新数据转换之前的结果
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        console.log(fetchMoreResult);
+        return {
+          userList: [
+            ...previousResult.userList,
+            ...fetchMoreResult.userList
+          ]
+        };
+      }
+    })
+  }
+  // created() {
+  //   this.getData()
+  // }
 }
 </script>
